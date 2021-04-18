39b6edf6aac3944c7d46fd1601bbe50b1ff04592
sakuraiyukina <sakuraiyukina0205@yahoo.co.jp>
2021-04-19 02:46:54 +0900

    Fix the problem that the file in the jar cannot be called

--- src/org/iHasherCore/Core.java
+++ src/org/iHasherCore/Core.java
@@ -4,12 +4,11 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 
 public class Core {
     public static void main(String[] args) {
-        Path file_help = Paths.get("src/resources/help");
+        Path file_help = (Path) ClassLoader.getSystemResourceAsStream("resources/help");
         try {
             List<String> help = Files.readAllLines(file_help, StandardCharsets.UTF_8);
             for (String s : help) {

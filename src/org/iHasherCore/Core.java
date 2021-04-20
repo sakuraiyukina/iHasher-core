e367b83f909f55b6a3105d4e83b061806013e0f8
sakuraiyukina <sakuraiyukina0205@yahoo.co.jp>
2021-04-21 00:17:27 +0900

    Fix the problem that the return value of the hash calculation function is empty.

--- src/org/iHasherCore/Core.java
+++ src/org/iHasherCore/Core.java
@@ -1,13 +1,17 @@
 package org.iHasherCore;
 
+import org.iHasherCore.hash.MD5;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.security.NoSuchAlgorithmException;
+
 
 public class Core {
     public static void main(String[] args) {
-        if(args.length == 0) {
+        if(args.length == 0 || args.length > 3) {
             try {
                 InputStream file_help = Core.class.getResourceAsStream("/resources/help");
                 assert file_help != null;
@@ -15,11 +19,15 @@ public class Core {
                 for(int i = 0; i < help.read(); i++) {
                     System.out.println(help.readLine());
                 }
-            } catch (IOException exception) {
+            }
+            catch (IOException exception) {
                 exception.printStackTrace();
             }
         }
-        //code
-
+        try {
+            MD5.generateHash(args[0], args[1]);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/org/iHasherCore/hash/MD5.java
+++ src/org/iHasherCore/hash/MD5.java
@@ -1,4 +1,22 @@
 package org.iHasherCore.hash;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class MD5 {
+    public static void generateHash(String algorithm, String data) throws NoSuchAlgorithmException {
+        switch (algorithm) {
+            case "-f":
+                break;
+            case "-s":
+                MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+                byte[] metadata = messageDigest.digest(data.getBytes());
+                StringBuilder stringBuilder = new StringBuilder(new BigInteger(1, metadata).toString(16));
+                while (stringBuilder.length() < 32) {
+                    stringBuilder.insert(0, "0");
+                }
+                System.out.println(stringBuilder);
+        }
+    }
 }

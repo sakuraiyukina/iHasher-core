461c7658059470ede343465bdb27f88194b5629c
sakuraiyukina <sakuraiyukina0205@yahoo.co.jp>
2021-04-20 20:05:56 +0900

    update help doc

--- src/org/iHasherCore/Core.java
+++ src/org/iHasherCore/Core.java
@@ -1,14 +1,21 @@
 package org.iHasherCore;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Objects;
 
 public class Core {
     public static void main(String[] args) {
-        InputStream file_help = Core.class.getResourceAsStream("resources/help");
-        BufferedReader help = new BufferedReader(new InputStreamReader(Objects.requireNonNull(file_help)));
-        System.out.println(help);
+        try {
+            InputStream file_help = Core.class.getResourceAsStream("/resources/help");
+            assert file_help != null;
+            BufferedReader help = new BufferedReader(new InputStreamReader(file_help));
+            for(int i = 0; i < help.read(); i++) {
+                System.out.println(help.readLine());
+            }
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
     }
 }
